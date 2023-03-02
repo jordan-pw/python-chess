9890ff94f34d4f539f7f7d60bef8303378884451
Jordan <jordanwright84@gmail.com>
2023-03-02 14:11:28 -0600

    refactored moves.py

--- chess.py
+++ chess.py
@@ -9,7 +9,7 @@ chessboard = [
     [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
     [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
     [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', 'P', ' ', ' ', ' ', ' ', ' ', ' '],
+    ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
     ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']]
 
 b = board.Board()
@@ -19,4 +19,6 @@ b.print_board()
 wp, wn, wb, wr, wq, wk, bp, bn, bb, br, bq, bk = b.get_all_bitboards()
 history = []
 history.append(moves.Move(52, 34, moves.MoveType.PAWN_DOUBLE))
-moves.possible_moves_white(history, wp, wn, wb, wr, wq, wk, bp, bn, bb, br, bq, bk)
+
+white_moves = moves.WhiteMoves(wp, wn, wb, wr, wq, wk, bp, bn, bb, br, bq, bk)
+white_moves.possible_moves(history, wp, wn, wb, wr, wq, wk, bp, bn, bb, br, bq, bk)
--- moves.py
+++ moves.py
@@ -2,10 +2,6 @@ import bitscan
 import numpy as np
 from enum import Enum
 
-import time
-
-leading_one = np.uint64(9223372036854775808)
-
 file_a = np.uint64(72340172838076673)
 file_h = np.uint64(9259542123273814144)
 rank_1 = np.uint64(255)
@@ -13,12 +9,6 @@ rank_4 = np.uint64(4278190080)
 rank_5 = np.uint64(1095216660480)
 rank_7 = np.uint64(71776119061217280)
 rank_8 = np.uint64(18374686479671623680)
-
-white_noncapturable = np.uint64()
-black_pieces = np.uint64()
-empty = np.uint64()
-
-test = False
 
 class Move:
     def __init__(self, from_square, to_square, move_type, promoted_piece=None):
@@ -46,101 +36,135 @@ class Promotion(Enum):
     ROOK = 3
     BISHOP = 4
 
-def possible_moves_white(history, wp, wn, wb, wr, wq, wk, bp, bn, bb, br, bq, bk):
-    global white_noncapturable
-    global black_pieces
-    global empty
-    white_noncapturable = ~(wp|wn|wb|wr|wq|wk|bk)
-    black_pieces = bp|bn|bb|br|bq
-    empty = ~(wp|wn|wb|wr|wq|wk|bp|bn|bb|br|bq|bk)
 
-    move_list = possible_pmoves_white(history, wp, bp)
-    print(move_list)
+class WhiteMoves:
+    def __init__(self, wp, wn, wb, wr, wq, wk, bp, bn, bb, br, bq, bk):
+        self.white_noncapturable = ~(wp|wn|wb|wr|wq|wk|bk)
+        self.black_pieces = bp|bn|bb|br|bq
+        self.empty = ~(wp|wn|wb|wr|wq|wk|bp|bn|bb|br|bq|bk)
 
-def possible_pmoves_white(history, wp, bp):
-    move_list = list()
+    def possible_moves(self, history, wp, wn, wb, wr, wq, wk, bp, bn, bb, br, bq, bk):
+        move_list = self.possible_pawn_moves(history, wp, bp)
+        print(move_list)
 
-    pawn_moves = np.uint64()
+    def possible_pawn_moves(self, history, wp, bp):
+        move_list = list()
 
-    # Moves and captures
-    # Capture right
-    pawn_moves = northeast_one(wp) & black_pieces & ~rank_8 & ~file_a
-    target_squares = bitscan.square_index_serialization(pawn_moves)
-    for target in target_squares:
-        start_square = target - 9
-        move_list.append(Move(start_square, target, MoveType.CAPTURE))
+        captures = self.pawn_captures(wp)
+        pushes = self.pawn_pushes(wp)
+        pawn_promotions = self.pawn_promotions(wp)
+        en_passant = self.pawn_en_passant(history, wp, bp)
 
-    # Capture left
-    pawn_moves = northwest_one(wp) & black_pieces & ~rank_8 & ~file_h
-    target_squares = bitscan.square_index_serialization(pawn_moves)
-    for target in target_squares:
-        start_square = target - 7
-        move_list.append(Move(start_square, target, MoveType.CAPTURE))
+        move_list = captures + pushes + pawn_promotions + en_passant
+        return move_list
 
-    # Move forward 1
-    pawn_moves = north_one(wp) & empty & ~rank_8
-    target_squares = bitscan.square_index_serialization(pawn_moves)
-    for target in target_squares:
-        start_square = target - 8
-        move_list.append(Move(start_square, target, MoveType.NORMAL))
+    def pawn_captures(self, wp):
 
-    # Move forward 2
-    pawn_moves = north_one(north_one(wp)) & empty & (empty << np.uint64(8)) & rank_4
-    target_squares = bitscan.square_index_serialization(pawn_moves)
-    for target in target_squares:
-        start_square = target - 16
-        move_list.append(Move(start_square, target, MoveType.PAWN_DOUBLE))
+        move_list = list()
 
-    # Promotions
-    # Promotion by capture right
-    pawn_moves = northeast_one(wp) & black_pieces & rank_8 & ~file_a
-    target_squares = bitscan.square_index_serialization(pawn_moves)
-    for target in target_squares:
-        start_square = target - 9
-        move_list.append(Move(start_square, target, MoveType.PROMOTION, Promotion.QUEEN))
-        move_list.append(Move(start_square, target, MoveType.PROMOTION, Promotion.KNIGHT))
-        move_list.append(Move(start_square, target, MoveType.PROMOTION, Promotion.ROOK))
-        move_list.append(Move(start_square, target, MoveType.PROMOTION, Promotion.BISHOP))
+        pawn_moves = np.uint64()
+        # Capture right
+        pawn_moves = northeast_one(wp) & self.black_pieces & ~rank_8 & ~file_a
+        target_squares = bitscan.square_index_serialization(pawn_moves)
+        for target in target_squares:
+            start_square = target - 9
+            move_list.append(Move(start_square, target, MoveType.CAPTURE))
 
-    # Promotion by capture left
-    pawn_moves = northwest_one(wp) & black_pieces & rank_8 & ~file_h
-    target_squares = bitscan.square_index_serialization(pawn_moves)
-    for target in target_squares:
-        start_square = target - 7
-        move_list.append(Move(start_square, target, MoveType.PROMOTION, Promotion.QUEEN))
-        move_list.append(Move(start_square, target, MoveType.PROMOTION, Promotion.KNIGHT))
-        move_list.append(Move(start_square, target, MoveType.PROMOTION, Promotion.ROOK))
-        move_list.append(Move(start_square, target, MoveType.PROMOTION, Promotion.BISHOP))
+        # Capture left
+        pawn_moves = northwest_one(wp) & self.black_pieces & ~rank_8 & ~file_h
+        target_squares = bitscan.square_index_serialization(pawn_moves)
+        for target in target_squares:
+            start_square = target - 7
+            move_list.append(Move(start_square, target, MoveType.CAPTURE))
 
-    # Promotion by move forward 1
-    pawn_moves = north_one(wp) & black_pieces & rank_8
-    target_squares = bitscan.square_index_serialization(pawn_moves)
-    for target in target_squares:
-        start_square = target - 8
-        move_list.append(Move(start_square, target, MoveType.PROMOTION, Promotion.QUEEN))
-        move_list.append(Move(start_square, target, MoveType.PROMOTION, Promotion.KNIGHT))
-        move_list.append(Move(start_square, target, MoveType.PROMOTION, Promotion.ROOK))
-        move_list.append(Move(start_square, target, MoveType.PROMOTION, Promotion.BISHOP))
+        return move_list
+
+    def pawn_pushes(self, wp):
+
+        move_list = list()
+
+        pawn_moves = np.uint64()
+        # Push forward 1
+        pawn_moves = north_one(wp) & self.empty & ~rank_8
+        target_squares = bitscan.square_index_serialization(pawn_moves)
+        for target in target_squares:
+            start_square = target - 8
+            move_list.append(Move(start_square, target, MoveType.NORMAL))
+
+        # Push forward 2
+        pawn_moves = north_one(north_one(wp)) & self.empty & (self.empty << np.uint64(8)) & rank_4
+        target_squares = bitscan.square_index_serialization(pawn_moves)
+        for target in target_squares:
+            start_square = target - 16
+            move_list.append(Move(start_square, target, MoveType.PAWN_DOUBLE))
+
+        return move_list
     
-    # En Passant 
-    if history:
-        if history[-1].move_type == MoveType.PAWN_DOUBLE:
-            pawn_moves = east_one(wp) & bp & rank_5 & ~file_a
-            target_squares = bitscan.square_index_serialization(pawn_moves)
-            for target in target_squares:
-                start_square = target - 1
-                end_square = target + 8
-                if target == history[-1].to_square:
-                    move_list.append(Move(start_square, end_square, MoveType.EN_PASSANT))
+    def pawn_promotions(self, wp):
 
-            pawn_moves = west_one(wp) & bp & rank_5 & ~file_h
-            target_squares = bitscan.square_index_serialization(pawn_moves)
-            for target in target_squares:
-                start_square = target + 1
-                end_square = target + 8
-                if target == history[-1].to_square:
-                    move_list.append(Move(start_square, end_square, MoveType.EN_PASSANT))
-    return move_list
+        move_list = list()
+
+        pawn_moves = np.uint64()
+        # Promotion by capture right
+        pawn_moves = northeast_one(wp) & self.black_pieces & rank_8 & ~file_a
+        target_squares = bitscan.square_index_serialization(pawn_moves)
+        for target in target_squares:
+            start_square = target - 9
+            move_list.append(Move(start_square, target, MoveType.PROMOTION, Promotion.QUEEN))
+            move_list.append(Move(start_square, target, MoveType.PROMOTION, Promotion.KNIGHT))
+            move_list.append(Move(start_square, target, MoveType.PROMOTION, Promotion.ROOK))
+            move_list.append(Move(start_square, target, MoveType.PROMOTION, Promotion.BISHOP))
+
+        # Promotion by capture left
+        pawn_moves = northwest_one(wp) & self.black_pieces & rank_8 & ~file_h
+        target_squares = bitscan.square_index_serialization(pawn_moves)
+        for target in target_squares:
+            start_square = target - 7
+            move_list.append(Move(start_square, target, MoveType.PROMOTION, Promotion.QUEEN))
+            move_list.append(Move(start_square, target, MoveType.PROMOTION, Promotion.KNIGHT))
+            move_list.append(Move(start_square, target, MoveType.PROMOTION, Promotion.ROOK))
+            move_list.append(Move(start_square, target, MoveType.PROMOTION, Promotion.BISHOP))
+
+        # Promotion by move forward 1
+        pawn_moves = north_one(wp) & self.black_pieces & rank_8
+        target_squares = bitscan.square_index_serialization(pawn_moves)
+        for target in target_squares:
+            start_square = target - 8
+            move_list.append(Move(start_square, target, MoveType.PROMOTION, Promotion.QUEEN))
+            move_list.append(Move(start_square, target, MoveType.PROMOTION, Promotion.KNIGHT))
+            move_list.append(Move(start_square, target, MoveType.PROMOTION, Promotion.ROOK))
+            move_list.append(Move(start_square, target, MoveType.PROMOTION, Promotion.BISHOP))
+
+        return move_list
+
+    def pawn_en_passant(self, history, wp, bp):
+
+        move_list = list()
+
+        pawn_moves = np.uint64()
+        # En Passant 
+        if history:
+            if history[-1].move_type == MoveType.PAWN_DOUBLE:
+                # En Passant Right
+                pawn_moves = east_one(wp) & bp & rank_5 & ~file_a
+                target_squares = bitscan.square_index_serialization(pawn_moves)
+                for target in target_squares:
+                    start_square = target - 1
+                    end_square = target + 8
+                    if target == history[-1].to_square:
+                        move_list.append(Move(start_square, end_square, MoveType.EN_PASSANT))
+
+                # En Passant Left
+                pawn_moves = west_one(wp) & bp & rank_5 & ~file_h
+                target_squares = bitscan.square_index_serialization(pawn_moves)
+                for target in target_squares:
+                    start_square = target + 1
+                    end_square = target + 8
+                    if target == history[-1].to_square:
+                        move_list.append(Move(start_square, end_square, MoveType.EN_PASSANT))
+
+        return move_list
+
 
 def north_one(bitboard):
     return bitboard << np.uint64(8)
